94e1c486ff1e3698096eae8e55642fe2e233bb2a
Kawashima Ryouma <rrisland21@gmail.com>
2023-06-04 19:20:19 +0900

    fix: upgrade default videojs version

--- videojs-video-element.js
+++ videojs-video-element.js
@@ -166,7 +166,7 @@ class VideojsVideoElement extends SuperVideoElement {
   }
 
   get version() {
-    return this.getAttribute('version') ?? '8.3.0';
+    return this.getAttribute('version') ?? '8.4.0';
   }
 }
 
